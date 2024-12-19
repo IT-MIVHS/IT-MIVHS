e9080892b03d170a81f13b7f9b61290e6993142b
cirengspicy <cirengspicy@gmail.com>
2024-12-19 08:17:27 +0700

    Animation Move Changes

--- src/components/robotComponent/robotComponents.jsx
+++ src/components/robotComponent/robotComponents.jsx
@@ -4,10 +4,17 @@ import { Typewriter } from "react-simple-typewriter";
 const RobotComponents = () => {
   return (
     <>
-      <img src="material.jpg" className="opacity-20 -z-50 absolute" alt="" />
-      <main id="robotsBody" className="relative flex flex-col items-center ">
+      <img
+        src="material.jpg"
+        className="opacity-20 -z-50 absolute h-1/2 w-full"
+        alt=""
+      />
+      <main
+        id="robotsBody"
+        className="relative flex flex-col items-center justify-center"
+      >
         {/* Typewriter Section */}
-        <div  className="text-center text-white text-2xl font-bold">
+        <div className="text-center text-white text-2xl font-bold">
           <Typewriter
             words={["Eat", "Sleep", "Code", "Repeat!"]}
             loop={true} // Loop sebanyak 5 kali
@@ -18,10 +25,10 @@ const RobotComponents = () => {
             delaySpeed={1000} // Waktu jeda sebelum mengetik ulang
           />
         </div>
-        
+
         {/* Head Section */}
         <section id="headRobot" className="mb-4">
-          <div className="border border-white/20 bg-indigo-700 w-32 h-20 rounded-3xl px-0 translate-y-3">
+          <div className="border border-white/20 bg-indigo-700 w-32 h-20 rounded-3xl px-0 translate-y-3 shadow-md shadow-white">
             <div
               id="containerEyes"
               className=" mx-auto border border-white flex items-center justify-between flex-row p-4 w-[6rem] h-3/4 translate-y-2.5 rounded-xl"
@@ -45,13 +52,13 @@ const RobotComponents = () => {
         {/* Body Section */}
         <section
           id="bodyRobot"
-          className="relative w-24 h-20 bg-indigo-700 border-2 border-white/20 rounded-b-[6rem] rounded-t-xl"
+          className="relative w-24 h-20 bg-indigo-700 border-2 border-white/20 rounded-b-[6rem] rounded-t-xl shadow-md shadow-white"
         >
           {/* Arms */}
-          <div className=" bg-indigo-700 rounded-full absolute -left-[2rem] -top-1 w-8 h-16 border border-white/20 transform rotate-45 -z-10">
+          <div className=" bg-indigo-700 rounded-full absolute -left-[2rem] -top-1 w-8 h-16 border border-white/20 transform rotate-45 -z-10 shadow-md shadow-white">
             <div className="bg-indigo-800 border border-white/20 w-full h-6 fixed bottom-0 rounded-full"></div>
           </div>
-          <div className="bg-indigo-700 rounded-full absolute -right-[2rem] -top-1 w-8 h-16 border border-white/20 transform -rotate-45 -z-10">
+          <div className="bg-indigo-700 rounded-full absolute -right-[2rem] -top-1 w-8 h-16 border border-white/20 transform -rotate-45 -z-10 shadow-md shadow-white">
             <div className="bg-indigo-800 border border-white/20 w-full h-6 fixed bottom-0 rounded-full"></div>
           </div>
 
@@ -61,11 +68,11 @@ const RobotComponents = () => {
         {/* Legs Section */}
         <section id="legsRobot" className="flex justify-between w-64 relative">
           {/* Left leg */}
-          <div className="w-8 h-24 bg-indigo-700 border border-white/20 rounded-full rotate-12 absolute left-[5rem] -top-16 -z-10">
+          <div className="w-8 h-24 bg-indigo-700 border border-white/20 rounded-full rotate-12 absolute left-[5rem] -top-16 -z-10 shadow-md shadow-white">
             <div className="bg-indigo-800 border border-white/20 w-full h-6 fixed bottom-0 rounded-full"></div>
           </div>
           {/* Right Leg */}
-          <div className="w-8 h-24 bg-indigo-700 border border-white/20 rounded-b-full -rotate-12 absolute right-[5rem] -top-16 -z-10">
+          <div className="w-8 h-24 bg-indigo-700 border border-white/20 rounded-b-full -rotate-12 absolute right-[5rem] -top-16 -z-10 shadow-md shadow-white">
             <div className="bg-indigo-800 border border-white/20 w-full h-6 fixed bottom-0 rounded-full"></div>
           </div>
         </section>
