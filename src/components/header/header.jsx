2b2a43bb4ab08303d1f7e3ce7f1ae67839f7c815
iwansal64 <iwansal64@gmail.com>
2025-01-13 14:15:07 +0700

    Fix Some Responsive Design Problems

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,65 +2,68 @@ import "./headerStyle.css";
 import React from "react";
 
 const Header = () => {
-  return (
-    <section>
-      <header
-        id="header"
-        className="fixed left-0 right-0 top-0 sm:top-0 sm:right-0 sm:left-0 mx-auto text-white z-50 w-5/6 sm:w-3/4 h-10 sm:h-full"
-      >
-        <nav className="sm:py-2 sm:px-4 py-0 px-4 flex justify-between sm:items-center items-start flex-row rounded-3xl">
-          <div id="bg"></div>
-          <div id="logos" className="flex items-center gap-5 rounded-full">
-            <img
-              id="logoIt"
-              src="teamItIcon.jpg"
-              alt="Icon Team IT"
-              className="rounded-full sm:w-8 sm:h-8 w-6 h-6"
-            />
-            <h1 id="logo" className="text-sm sm:text-2xl font-bold tracking-widest">
-              <span>IT</span>
-              <span>MIVHS</span>
-            </h1>
-          </div>
-          <div id="toggle">
-            <span></span>
-            <span></span>
-            <span></span>
-            <input type="checkbox" name="" id="" />
-          </div>
-          <ul
-            id="navigate"
-            className="flex gap-6 justify-end font-medium text-sm flex-col md:flex-row"
-          >
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li>
-              <a href="#project">Project</a>
-            </li>
-            <li>
-              <a href="#gallery">Gallery</a>
-            </li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
-            <li className="p-4 sm:p-0">
-              <a
-                id="signup"
-                className="bg-pink-600 py-4 px-3 sm:py-1 sm:px-3 rounded-3xl"
-                href="#regist"
-              >
-                Sign Up
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </section>
-  );
+    return (
+        <section>
+            <header
+                id="header"
+                className="fixed -translate-x-1/2 left-1/2 right-0 top-0 sm:top-0 sm:right-0  text-white z-50 w-5/6 sm:w-3/4 h-10 sm:h-full"
+            >
+                <nav
+                    id="navigation"
+                    className="sm:py-2 sm:px-4 py-0 px-4 flex justify-between sm:items-center flex-row rounded-3xl"
+                >
+                    <div id="bg"></div>
+                    <div id="logos" className="flex items-center gap-5 rounded-full">
+                        <img
+                            id="logoIt"
+                            src="teamItIcon.jpg"
+                            alt="Icon Team IT"
+                            className="rounded-full sm:w-8 sm:h-8 w-6 h-6"
+                        />
+                        <h1 id="logo" className="text-sm sm:text-2xl font-bold tracking-widest">
+                            <span>IT</span>
+                            <span>MIVHS</span>
+                        </h1>
+                    </div>
+                    <div id="toggle">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <input type="checkbox" name="" id="" />
+                    </div>
+                    <ul
+                        id="navigate"
+                        className="flex gap-6 justify-end font-medium text-sm flex-col"
+                    >
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#about">About</a>
+                        </li>
+                        <li>
+                            <a href="#project">Project</a>
+                        </li>
+                        <li>
+                            <a href="#gallery">Gallery</a>
+                        </li>
+                        <li>
+                            <a href="#contact">Contact</a>
+                        </li>
+                        <li className="p-4 sm:p-0">
+                            <a
+                                id="signup"
+                                className="bg-pink-600 py-4 px-3 sm:py-1 sm:px-3 rounded-3xl"
+                                href="#regist"
+                            >
+                                Sign Up
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </section>
+    );
 };
 
 export default Header;
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -3,62 +3,62 @@ import { Typewriter } from "react-simple-typewriter";
 import "./homeStyle.css";
 
 const Home = function () {
-  function join() {
-    console.log("TEST");
-  }
+    function join() {
+        console.log("TEST");
+    }
 
-  return (
-    <>
-      <section id="home" className="flex items-center text-white h-screen">
-        <div
-          id="container"
-          className="relative flex flex-col gap-4 justify-center sm:py-6 sm:px-28 py-8 px-4 w-full sm:h-fit h-1/2 translate-y-1/4 sm:translate-y-0"
-        >
-          <div id="material-bg">
-            <div
-              id="material-bg-image"
-              src="material.jpg"
-              className="-z-10 left-0"
-              alt="Example"
-            />
-          </div>
-          <p id="greetings" className="font-bold text-xl text-pink-400 w-fit">
-            Halo, Selamat Datang
-          </p>
-          <div className="font-bold text-white text-lg text-start">
-            <Typewriter
-              words={[
-                "Ayo!, Gabung Bersama Kami di",
-                "Team IT Mitra Industri MM2100",
-              ]}
-              loop={true} // Loop sebanyak 5 kali
-              cursor
-              cursorStyle="_"
-              typeSpeed={100} // Kecepatan mengetik
-              deleteSpeed={50} // Kecepatan menghapus
-              delaySpeed={1000} // Waktu jeda sebelum mengetik ulang
-            />
-          </div>
-          <p className="description text-justify w-4/5 text-sm sm:text-lg">
-            Kami mencari individu yang bersemangat, inovatif, dan memiliki
-            keinginan kuat untuk belajar dan berkontribusi dalam dunia IT.
-            Bersama kita menciptakan solusi teknologi untuk masa depan
-          </p>
+    return (
+        <>
+            <section id="home" className="flex items-center text-white h-screen">
+                <div
+                    id="container"
+                    className="relative flex flex-col gap-4 justify-center sm:py-6 sm:px-28 py-8 px-4 w-full sm:h-fit h-1/2 translate-y-1/4 sm:translate-y-0"
+                >
+                    <div id="material-bg">
+                        <div
+                            id="material-bg-image"
+                            src="material.jpg"
+                            className="-z-10 left-0"
+                            alt="Example"
+                        />
+                    </div>
+                    <p id="greetings" className="font-bold text-xl text-pink-400 w-fit">
+                        Halo, Selamat Datang
+                    </p>
+                    <div className="font-bold text-white text-lg text-start">
+                        <Typewriter
+                            words={[
+                                "Ayo!, Gabung Bersama Kami di",
+                                "Team IT Mitra Industri MM2100",
+                            ]}
+                            loop={true} // Loop sebanyak 5 kali
+                            cursor
+                            cursorStyle="_"
+                            typeSpeed={100} // Kecepatan mengetik
+                            deleteSpeed={50} // Kecepatan menghapus
+                            delaySpeed={1000} // Waktu jeda sebelum mengetik ulang
+                        />
+                    </div>
+                    <p className="description text-justify w-4/5 text-sm sm:text-lg">
+                        Kami mencari individu yang bersemangat, inovatif, dan memiliki keinginan
+                        kuat untuk belajar dan berkontribusi dalam dunia IT. Bersama kita
+                        menciptakan solusi teknologi untuk masa depan
+                    </p>
 
-          <button
-            id="join"
-            className="self-start w-1/2 border-black px-6 py-2 rounded-xl mt-2 bg-violet-600 hover:bg-violet-900 hover:brightness-75 duration-100 text-sm font-bold"
-            onClick={join}
-          >
-            Daftar
-          </button>
-        </div>
-        <div>
-          <div id="image"></div>
-        </div>
-      </section>
-    </>
-  );
+                    <button
+                        id="join"
+                        className="self-start w-1/2 border-black px-6 py-2 rounded-xl mt-2 bg-violet-600 hover:bg-violet-900 hover:brightness-75 duration-100 text-sm font-bold"
+                        onClick={join}
+                    >
+                        Daftar
+                    </button>
+                </div>
+                <div>
+                    <div id="image"></div>
+                </div>
+            </section>
+        </>
+    );
 };
 
 export default Home;
